255d2d0a46d7bae6932d31d07172cce4fcd0d74c
GripFast-III <jeandbdp@gmail.com>
2023-02-13 16:55:15 +0100

    Modal ouvrante, submit, fermer modal + text min

--- modal.js
+++ modal.js
@@ -7,6 +7,10 @@ function editNav() {
   }
 }
 
+function validate(event) {
+  alert("Merci pour votre inscription")
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -20,4 +24,44 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal(event) {
+  modalbg.style.display = "none";
+}
 
+/*
+// Prénom
+const inputFirst = document.querySelector("#first");
+const checkFirst = (event) => {
+  console.log("first", event)
+}
+inputFirst.addEventListener("input",checkFirst);
+
+// Nom
+const inputLast = document.querySelector("#last");
+const checkLast = (event) => {
+  console.log("last", event)
+}
+inputLast.addEventListener("input",checkLast);
+
+// email
+const inputEmail = document.querySelector("#email");
+const checkEmail = (event) => {
+  console.log("email", event)
+}
+inputEmail.addEventListener("input",checkEmail);
+
+// Birthdate
+const inputBirthDate = document.querySelector("#birthDate");
+const checkBirthDate = (event) => {
+  console.log("birthdate", event)
+}
+inputBirthDate.addEventListener("input",checkBirthDate);
+
+// Quantity
+const inputQuantity = document.querySelector("#quantity");
+const checkQuantity = (event) => {
+  console.log("quantity", event)
+}
+inputQuantity.addEventListener("input",checkQuantity);
+*/
